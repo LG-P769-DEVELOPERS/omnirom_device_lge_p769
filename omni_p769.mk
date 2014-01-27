2f9d7ee28e51f6ba1fd94c8a09d09e5b6379eed1
TWINNFAMOUS <twinnfamous@gmail.com>
2014-01-27 04:01:20 +0000

    Update omni_p769.mk

--- omni_p769.mk
+++ omni_p769.mk
@@ -16,25 +16,21 @@
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/p769/p769.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u2_open_eu BUILD_FINGERPRINT=lge/u2_open_eu/u2:4.1.2/JRO03L/P76020b.4DAAAC8F:user/release-keys PRIVATE_BUILD_DESC="u2_open_eu-user 4.1.2 JRO03L P76020b.4DAAAC8F release-keys"
+$(call inherit-product, device/lge/p769/full_p769.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p769
 
-# Enable Torch
-PRODUCT_PACKAGES += Torch LgeL9Parts
- 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := omni_p769
 PRODUCT_DEVICE := p769
 PRODUCT_MODEL := LG-P769
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u2_open_eu BUILD_FINGERPRINT=lge/u2_open_eu/u2:4.1.2/JRO03L/P76020b.4DAAAC8F:user/release-keys PRIVATE_BUILD_DESC="u2_open_eu-user 4.1.2 JRO03L P76020b.4DAAAC8F release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch LgeL9Parts
